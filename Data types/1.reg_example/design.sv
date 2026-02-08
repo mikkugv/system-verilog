4e91cd5e321119c483f9f63ce7f3abbef8f23899
mikkugv <92909499+mikkugv@users.noreply.github.com>
2026-02-08 21:24:26 +0530

    Create design.sv

---
+++ Data types/1.reg_example/design.sv
@@ -0,0 +1,13 @@
+module reg_example(
+  input logic clk,
+  input logic rst,
+  input logic d, 
+  output logic q
+);
+  always_ff @(posedge clk or posedge rst) begin
+    if(rst)
+      q<=1'b0;
+    else
+      q<=d;
+  end
+endmodule

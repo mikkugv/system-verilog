be469b9acb0b030ceac2dcf1605cb8659a008ac4
mikkugv <92909499+mikkugv@users.noreply.github.com>
2026-03-16 15:37:59 +0530

    Add comments for simulation output in packed_array_example
    Added simulation output comments for clarity.

--- Array/Question 1/packed_array_example.sv
+++ Array/Question 1/packed_array_example.sv
@@ -8,3 +8,20 @@ $display("Value of pqr = %h", pqr);
 $display("Value of xyz = %d", xyz);
 end
 endmodule
+
+/*output
+[2026-03-16 10:06:17 UTC] xrun -Q -unbuffered '-timescale' '1ns/1ns' '-sysv' '-access' '+rw' design.sv testbench.sv  
+TOOL:	xrun	25.03-s001: Started on Mar 16, 2026 at 06:06:17 EDT
+xrun: 25.03-s001: (c) Copyright 1995-2025 Cadence Design Systems, Inc.
+	Top level design units:
+		packed_array_example
+Loading snapshot worklib.packed_array_example:sv .................... Done
+xcelium> source /xcelium25.03/tools/xcelium/files/xmsimrc
+xcelium> run
+Value of abc = 0110
+Value of pqr = 10fe
+Value of xyz =  16
+xmsim: *W,RNQUIE: Simulation is complete.
+xcelium> exit
+TOOL:	xrun	25.03-s001: Exiting on Mar 16, 2026 at 06:06:19 EDT  (total: 00:00:02)
+Done */
